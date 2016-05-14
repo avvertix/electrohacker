2fc231d33b301a1523df28377070528dd8f33dc5
alessio <alessio.vertemati@gmail.com>
2016-05-14 21:33:11 +0200

    updated Electron to version 1.0

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 /// <reference path="typings/node/node.d.ts"/>
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-const crashReporter = require('electron').crashReporter;
-
-
-// Report crashes to our server.
-crashReporter.start({
-  productName: 'ElectroHacker',
-  companyName: 'AVSoft',
-  submitURL: 'https://avsoft.it/electrohacker-crash',
-  autoSubmit: false
-});
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
-var mainWindow = null;
-
-// Quit when all windows are closed.
-
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -25,32 +15,46 @@ app.on('window-all-closed', function() {
   }
 });
 
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
-app.on('ready', function() {
+function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-      width: 800, 
-      height: 600, 
-      minWidth: 420,
-      minHeight: 400,
-      icon: 'images/logo.png'
-  });
+    width: 800,
+    height: 600,
+    minWidth: 420,
+    minHeight: 400,
+    icon: 'images/logo.png'
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  
-  // app.addRecentDocument('C:/ciao.txt');
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-//mainWindow.setProgressBar(0.5);
-  // Open the devtools.
-  // mainWindow.openDevTools();
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-});
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
